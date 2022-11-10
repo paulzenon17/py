0ffffb54e597c63510724f41d47e19fe3975fb01
paulzenon17 <96942377+paulzenon17@users.noreply.github.com>
2022-11-10 21:20:21 +0530

    Update 11_dict_exercise_3_circle.py
     **Use this alternative easy code** 
        
        def circle_calc(a):
        area=(22/7)*a*a
        circum=2*(22/7)*a
        dia=2*a
        z1=f'\n{area}\n{round(circum)}\n{dia}'
        z2=print('The Area, Circumference & Diameter of the circle is:',z1)
        return z2

--- Basics/Exercise/11_dict_tuples/11_dict_exercise_3_circle.py
+++ Basics/Exercise/11_dict_tuples/11_dict_exercise_3_circle.py
@@ -11,3 +11,13 @@ if __name__=="__main__":
     r=float(r)
     area, c, d = circle_calc(r)
     print(f"area {area}, circumference {c}, diameter {d}")
+    
+     **Use this alternative easy code** 
+    
+    def circle_calc(a):
+    area=(22/7)*a*a
+    circum=2*(22/7)*a
+    dia=2*a
+    z1=f'\n{area}\n{round(circum)}\n{dia}'
+    z2=print('The Area, Circumference & Diameter of the circle is:',z1)
+    return z2
